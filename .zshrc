5f9b1a9027ec248cdc69a93609c2bed33b18f680
8888 <lee@rubiconmd.com>
2020-06-27 11:04:50 -0400

    initial .zshrc
    still sourcing old one while it gets cleaned up and migrated

---
+++ .zshrc
@@ -0,0 +1,3 @@
+# source old .zshrc
+# remove after everything is cleaned up and migrated
+source ~/.old_zshrc
--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ ln -s ${dir}/.bash_profile ~/.bash_profile
 ln -s ${dir}/.gitconfig ~/.gitconfig
 ln -s ${dir}/.gitignore_global ~/.gitignore_global
 ln -s ${dir}/.vimrc ~/.vimrc
+ln -s ${dir}/.zshrc ~/.zshrc
