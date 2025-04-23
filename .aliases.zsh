cb727b246e6209838146252d804edc7ae386f98f
8888 <leecostello1@gmail.com>
2025-04-23 18:23:30 -0400

    feat: add uar alias for updating multiple git repos

--- .aliases.zsh
+++ .aliases.zsh
@@ -13,6 +13,7 @@ alias utcdate='date -u +%FT%TZ'
 # https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/git/git.plugin.zsh
 # if any of the above are not available, run `upgrade_oh_my_zsh`
 alias gstsu='git stash save --include-untracked' #[<message>]
+alias uar='update_all_repos'
 
 # Docker aliases
 alias dcu='docker compose up -d --remove-orphans'
@@ -41,5 +42,23 @@ alias dumpling='bundle exec ~/Documents/code/rmd/rubicon/bin/dumpling'
 function sak() {
   for filename in ~/.ssh/*.pem; do
     ssh-add --apple-use-keychain "$filename"
+  done
+}
+
+# Update all git repositories in current directory and one level deep
+function update_all_repos() {
+  # Update current directory if it's a git repo
+  if [ -d .git ]; then
+    echo "Updating $(pwd)"
+    gfa
+    gl
+  fi
+
+  # Update all git repos one level deep
+  for dir in */; do
+    if [ -d "$dir/.git" ]; then
+      echo "Updating $dir"
+      (cd "$dir" && gfa && gl)
+    fi
   done
 }
