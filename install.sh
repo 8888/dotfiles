13795e44850028ba9b2181642396fcea44731313
8888 <leecostello1@gmail.com>
2025-06-09 20:59:34 -0400

    fix: prevent recursive symlinking by removing existing ~/.lee directory in install script

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ ln -s ${dir}/.bash_profile ~/.bash_profile
 ln -s ${dir}/.gitconfig ~/.gitconfig
 ln -s ${dir}/.gitignore_global ~/.gitignore_global
 ln -s ${dir}/.p10k.zsh ~/.p10k.zsh
+rm -rf ~/.lee # prevent recursive symlinking
 ln -s ${dir}/.lee ~/.lee
 ln -s ${dir}/.vimrc ~/.vimrc
 ln -s ${dir}/.zshrc ~/.zshrc
